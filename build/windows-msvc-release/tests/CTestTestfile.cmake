beb0e1c7475831bbb57a74fb3f414d86c0c594c5
gomurin0428 <gomurin0428@gmail.com>
2025-04-20 19:57:37 +0900

    Delete build/windows-msvc-release directory

--- build/windows-msvc-release/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/iconv-alt/tests
-# Build directory: C:/Dev/iconv-alt/build/windows-msvc-release/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[smoke]=] "C:/Dev/iconv-alt/build/windows-msvc-release/tests/smoke.exe")
-set_tests_properties([=[smoke]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Dev/iconv-alt/tests/CMakeLists.txt;3;add_test;C:/Dev/iconv-alt/tests/CMakeLists.txt;0;")
